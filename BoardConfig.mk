ebe2a6ab9a0f68bde9f6b32c9e9910b0bff8249b
Termux <Harshjangrap@gmail.com>
2026-05-19 19:40:59 +0530

    lisa: fix Statusbar Alignment

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,8 +157,8 @@ TW_MAX_BRIGHTNESS := 2047
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone26/temp
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_THEME := portrait_hdpi
-TW_Y_OFFSET := 100
-TW_H_OFFSET := -100
+TW_Y_OFFSET := 48
+TW_H_OFFSET := -48
 
 TW_HAS_EDL_MODE := true
 TW_ENABLE_BLKDISCARD := true
